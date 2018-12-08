0c7f23015d0c03c368f294150dd01dd6ea981e81
Rafiul Islam <avoidcloud@gmail.com>
2018-12-08 10:27:17 +0600

    init index structure

--- alpha/urls.py
+++ alpha/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.conf.urls import include, url
 from django.urls import path
+from home import views as index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+	
+	# managed all index functionality in home
+    url(r'^$', include('home.urls')),
 ]
---
+++ home/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from django.conf.urls import url
+from home import views
+
+
+urlpatterns = [
+	
+	url(r'^$', views.index, name='index')
+]
