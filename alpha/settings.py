8fdf56884cb90e197b3a774141110aa20f9b2f60
Rafiul Islam <avoidcloud@gmail.com>
2018-12-10 16:48:31 +0600

    Update settings.py

--- alpha/settings.py
+++ alpha/settings.py
@@ -121,5 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 #STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collection')
 STATIC_URL = '/static/'
