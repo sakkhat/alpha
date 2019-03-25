884bc88f5cf47f7fae65affaafc115088f8ca116
Rafiul Islam <avoidcloud@gmail.com>
2019-03-25 21:37:55 +0600

    refactoring...

---
+++ static/js/generic/slider.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('.product-slick-container').slick({
+		dots: false,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		adaptiveHeight: true,
+		autoplay:true,
+		autoplaySpeed:1500,
+		slidesToShow: 1,
+		centerMode: true,
+		variableWidth: true,
+		arrows:false
+	});
+});
