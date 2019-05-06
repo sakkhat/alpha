9407509dfd54972a4b03baa5cc0b99d3dc13cfc1
Rafiul Islam <avoidcloud@gmail.com>
2019-05-06 16:18:13 +0600

    refactoring...

--- alpha/settings.py
+++ alpha/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ["localhost", "192.168.1.11", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
